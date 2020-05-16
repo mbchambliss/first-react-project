3c0144cfca377a1abbe722acb6a3e5196f052003
blakechambliss <mbchambliss@gmail.com>
2020-05-16 13:23:17 -0500

    Updates

---
+++ precache-manifest.80a1c0eb0ecd9d4c371c2ed25a2588c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04db018d79e4250527b72f3daacc5ee6",
+    "url": "/first-react-project/index.html"
+  },
+  {
+    "revision": "4f891eab70ff740c1496",
+    "url": "/first-react-project/static/css/main.c02799e4.chunk.css"
+  },
+  {
+    "revision": "40f4b1cb613365bea0f8",
+    "url": "/first-react-project/static/js/2.87d12c6b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/first-react-project/static/js/2.87d12c6b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f891eab70ff740c1496",
+    "url": "/first-react-project/static/js/main.5546cfb2.chunk.js"
+  },
+  {
+    "revision": "a6506b25fc94c76d8321",
+    "url": "/first-react-project/static/js/runtime-main.53fe3c2a.js"
+  }
+]);
